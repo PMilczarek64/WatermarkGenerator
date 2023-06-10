82d6ff770946a2ca6354c2632c2c284dd39e5268
Mammoth64 <mammoth1907@gmail.com>
2023-06-10 12:48:51 +0200

    Add app.js - creating centered watermark using image

---
+++ app.js
@@ -0,0 +1,17 @@
+const Jimp = require('jimp');
+
+const addTextWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
+  const image = await Jimp.read(inputFile);
+  const watermark = await Jimp.read(watermarkFile);
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+  image.composite(watermark, x, y, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
+ 
+  await image.quality(100).writeAsync(outputFile);
+};
+
+addTextWatermarkToImage('./test.jpg', './test-with-watermark2.jpg', './logo.png');
